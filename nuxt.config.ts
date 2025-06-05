90c21cd1c0f96cf0e22fbc1f1b8d6b525a8df96b
shadowdreamer <git@dovahkiin.top>
2025-06-05 12:26:59 +0800

    ✨ feat: prerender

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,9 +44,8 @@ export default defineNuxtConfig({
   nitro: {
     preset: 'node-server',
     routeRules: {
-     '/': {
-       prerender: true,
-     } 
+     '/': { prerender: true, } ,
+     'gugugaga': { prerender: true, } ,
     },
     esbuild: {
       options: {
